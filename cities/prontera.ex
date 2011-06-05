9123e5c52bc640dd32ec5140d14f17049e918426
Bryan Veloso <bryan@revyver.com>
2011-06-04 20:15:06 -0700

    Couple more Prontarians.

--- cities/prontera.ex
+++ cities/prontera.ex
@@ -29,19 +29,19 @@ object Shuger
   }
 
   def main
-    say "[Shuger]"
+    say display_name
     say "Outside the safety of the city, there is a pink beast known as " + colored("000077", "poring") + "."
     next
 
-    say "[Shuger]"
+    say display_name
     say "Though it's cute in appearance and does not actively harm people, Poring is known to absorb items that are on the groung into its own body."
     next
 
-    say "[Shuger]"
+    say display_name
     say "So if there's something on the ground that you want to pick up, be careful lest it be consumed by a Poring. Then again... Porings are pretty weak."
     next
 
-    say "[Shuger]"
+    say display_name
     say "The green colored " + colored("000077", "Poporing") + "is tougher than Poring. Newbies generally make the mistake of attacking it without being aware of its power... So be careful!"
     close
   end
@@ -61,24 +61,82 @@ object Tono
   }
 
   def main
-    say "[Tono]"
+    say display_name
     say "Did you know?"
     next
 
-    say "[Tono]"
+    say display_name
     say "The larve of Creamy is Fabre. So, those green little wormy things are actually the babies of those pinkish, purply butterfly things you see around."
     next
 
-    say "[Tono]"
+    say display_name
     say "But before Fabres can become Creamys, they go into a pupa stage. When that happens, they turn into these dark purple cocoons we call Pupa. Simple, huh?"
     next
 
-    say "[Tono]"
+    say display_name
     say "There's another monster that goes through a really big change... Pickys."
     next
 
-    say "[Tono]"
+    say display_name
     say "Pickys are so cute when they're young, but when they grow up, they turn tinto those big, gawky looking Peco Pecos. Talk about awkward puberty."
     close
   end
 end
+
+
+object Merideth
+  mixin NPC
+
+  name "Meredith"
+  sprite 91
+
+  register {
+    'map: "prontera",
+    'coordinates: {106, 116},
+    'direction: 'east
+  }
+
+  def main
+    say display_name
+    say "The weather was nice on my day off so my family and I went for a picnic. We chose to got a slightly secluded area where I saw something really interesting..."
+    next
+
+    say display_name
+    say "It was a large group of Giant Hornets! What was even weirder was that they were all controlled by this one Queen Bee, following her every command."
+    next
+
+    say display_name
+    say "They might jus be bugs, but I think they've got the right idea. Men really ought to take commands from us women... We do things right!"
+    close
+  end
+end
+
+
+object YuPi
+  mixin NPC
+
+  name "YuPi"
+  sprite 102
+
+  register {
+    'map: "prontera",
+    'coordinates: {160, 133},
+    'direction: 'west
+  }
+
+  def main
+    say display_name
+    say "Although many monsters may look the same, be careful! There are variations among monsters that have the same basic form."
+    next
+
+    say display_name
+    say "One monster, that looks just like a peaceful and weak one that you've already encountered, may actually be wild and ferocious!"
+    next
+
+    say display_name
+    say "You can tell these kinds of monsters apart by their body color. Wilder and more powerful monsters have more dangerous looking colors."
+    close
+  end
+end
+
+
